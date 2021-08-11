1ae053e00caaabe43c3a06f127542f80c59d0ee5
Vishu Tomar <54905178+vishutomar03@users.noreply.github.com>
2021-08-11 16:00:38 +0530

    Time: 26 ms, Space: 55.6 MB

--- 1838. Frequency of the Most Frequent Element/1838. Frequency of the Most Frequent Element.java
+++ 1838. Frequency of the Most Frequent Element/1838. Frequency of the Most Frequent Element.java
@@ -1,8 +1,8 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        Arrays.sort(nums);
         int n = nums.length;
         long[] suffix = new long[n];
+        Arrays.sort(nums);
         suffix[n-1] = nums[n-1];
         
         for(int i = n-2; i >=0;i --){
@@ -14,9 +14,9 @@ class Solution {
         for(int i = 0; i < n; i++){
             l = -1;
             r = i;
-            while(l+1<r){
+            while(l + 1 < r){
                 mid = (l + r)/2;
-                if(suffix[mid] + k  - suffix[i] <(long) nums[i] * (long)(i - mid)){
+                if(suffix[mid] + k  - suffix[i] < (long) nums[i] * (long)(i - mid)){
                     l = mid;
                 }
                 else{
