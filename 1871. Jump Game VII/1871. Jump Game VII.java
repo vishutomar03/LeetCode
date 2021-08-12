26bfb50f9e1ccc59d59506629468aeb91a67f32f
Vishu Tomar <54905178+vishutomar03@users.noreply.github.com>
2021-08-12 23:24:05 +0530

    Add files via upload

---
+++ 1871. Jump Game VII/1871. Jump Game VII.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        char[] arr = s.toCharArray();
+        int n = arr.length, a = 0, b = 0;
+        ArrayList<Integer> ar = new ArrayList<>();
+        TreeSet<Integer> set = new TreeSet<>();
+    
+        for(int i = n-2; i >= 0; i--){
+            if(arr[i]=='0'){
+                ar.add(i);
+            }
+        }
+        
+        if(arr[n-1] !='0'){
+            return false;
+        }
+        
+        set.add(n-1);
+        int l, r, mid;
+        
+        for(Integer i : ar){
+            if(set.ceiling(i + minJump)!=null){
+                if(set.ceiling(i+minJump) <= i + maxJump){
+                    set.add(i);
+                }
+            }
+        }
+        
+        return set.contains(0);
+    }
+}
