8012c2c4eb247cec21f291ddf7e035c5adce6014
Vishu Tomar <54905178+vishutomar03@users.noreply.github.com>
2021-08-12 23:00:53 +0530

    Time: 2 ms, Space: 39.1 MB

--- 1849. Splitting a String Into Descending Consecutive Values/1849. Splitting a String Into Descending Consecutive Values.java
+++ 1849. Splitting a String Into Descending Consecutive Values/1849. Splitting a String Into Descending Consecutive Values.java
@@ -6,11 +6,10 @@ class Solution {
         arr = s.toCharArray();
         return ans(0, 0, -1, 0);
     }
-    
     public boolean ans(int index, int started, long prev, int a){
-        if(index>=arr.length){
-            if(started==index || started == 0||prev==-1){
-                if(a<=1){
+        if(index >= arr.length){
+            if(started == index || started == 0||prev == -1){
+                if(a <= 1){
                     return false;
                 }
                 return true;
@@ -20,14 +19,13 @@ class Solution {
             return false;
         }
         boolean opt = false;
-        if(Long.parseLong(s.substring(started, index+1)) < (long)1e10){
-            opt = ans(index+1, started, prev, a);
+        if(Long.parseLong(s.substring(started, index + 1)) < (long)1e10){
+            opt = ans(index + 1, started, prev, a);
         }
         else{
             return false;
         }
-    
-        if(prev==-1){
+        if(prev == -1){
             opt =  opt || ans(index+1, index+1, Long.parseLong(s.substring(started, index+1)), a + 1);
         }
         else{
