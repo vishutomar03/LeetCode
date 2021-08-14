7aed6bd3e0056fc8d9d090c2b0d20ba5509228eb
Vishu Tomar <54905178+vishutomar03@users.noreply.github.com>
2021-08-14 23:12:45 +0530

    Add files via upload

---
+++ 1755. Closest Subsequence Sum/1755. Closest Subsequence Sum.java
@@ -0,0 +1,55 @@
+class Solution {
+    int[] arr;
+    public int minAbsDifference(int[] nums, int goal) {
+        arr = nums;
+        int n = arr.length;
+        ArrayList<Long> array = new ArrayList<>();
+        ans(0,0,n/2, array);
+        ArrayList<Long> array1 = new ArrayList<>();
+        ans(n/2, 0, n, array1);
+        
+        Collections.sort(array);
+        Collections.sort(array1);
+    
+        int l, r, mid;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < array.size(); i++){
+            l = -1;
+            r = array1.size();
+            while(l+1<r){
+                mid = (l+r)/2;
+                if(array1.get(mid) <= goal - array.get(i)){
+                    l = mid;
+                }
+                else{
+                    r = mid;
+                }
+            }
+    
+            if(l!=-1){
+                min = (int)Math.min(min, Math.abs(array.get(i) + array1.get(l)  - goal));
+                if(r!=array1.size()){
+                    min = (int)Math.min(min, Math.abs(array.get(i) + array1.get(r)  - goal));
+                }
+            }
+            else{
+                if(r!=array1.size()){
+                    min = (int)Math.min(min, Math.abs(array.get(i) + array1.get(r)  - goal));
+                }
+            }
+            
+        }
+        
+        
+        return min;
+    }
+    public void ans(int index, long sum, int n, ArrayList<Long> array){
+        if(index>=n){
+            array.add(sum);
+            return;
+        }
+        
+        ans(index + 1, sum + arr[index], n, array);
+        ans(index + 1, sum, n, array);
+    }
+}
