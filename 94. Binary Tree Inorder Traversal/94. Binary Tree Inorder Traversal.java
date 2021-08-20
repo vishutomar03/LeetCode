4a2383e70e6c0bc06ab770618a8ae76c7e71a80a
Vishu Tomar <54905178+vishutomar03@users.noreply.github.com>
2021-08-20 23:25:01 +0530

    Add files via upload

---
+++ 94. Binary Tree Inorder Traversal/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if(root==null){
+            return new LinkedList<>();
+        }
+        stack.add(root);
+        while(!stack.isEmpty()){
+            while(root.left!=null){
+                stack.add(root.left);
+                root = root.left;
+            }
+            root = stack.pop();
+            list.add(root.val);
+            while(root.right==null && !stack.isEmpty()){
+                root = stack.pop();
+                list.add(root.val);
+            }            
+            if(root.right!=null){
+                stack.add(root.right);
+                root = root.right;
+            }
+        }
+        return list;
+    }
+}
