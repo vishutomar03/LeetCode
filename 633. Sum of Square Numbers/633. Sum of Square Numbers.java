8a760e2090e052dc9d84da0bbe75878c92c77090
Vishu Tomar <54905178+vishutomar03@users.noreply.github.com>
2021-08-25 23:59:09 +0530

    Time: 26 ms, Space: 35.8 MB

--- 633. Sum of Square Numbers/633. Sum of Square Numbers.java
+++ 633. Sum of Square Numbers/633. Sum of Square Numbers.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
         long a, b = c;
-        for(long i = 0; i*i <= c; i++){
-            a = b - i*i;
+        for(long i = 0; i * i <= c; i++){
+            a = b - i * i;
             if((long)Math.pow(a,0.5) * (long)Math.pow(a,0.5) == a)
                 return true;
         }
