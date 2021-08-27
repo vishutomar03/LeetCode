23adb696c7bc7b5671f76a1c42f3450b50649632
Vishu Tomar <54905178+vishutomar03@users.noreply.github.com>
2021-08-27 09:40:17 +0530

    Add files via upload

---
+++ 331. Verify Preorder Serialization of a Binary Tree/331. Verify Preorder Serialization of a Binary Tree.java
@@ -0,0 +1,46 @@
+class Solution {
+    ArrayList<Integer> arr = new ArrayList<>();
+    int n, a = 0, index = 0;
+    public boolean isValidSerialization(String preorder) {
+        n = preorder.length();
+        
+        for(int i = 0; i < n; i++){
+            if(preorder.charAt(i) == '#'){
+                arr.add(0);
+                i++;
+            }
+            else{
+                while(i < n && preorder.charAt(i) != ',')
+                    i++;                
+                arr.add(1);
+            }
+        }
+        
+        n = arr.size();
+        
+        if(n%2==0)
+            return false;
+        
+        ans();
+    
+        if(index + 1==n)
+            return true;
+        return false;
+        
+    }
+    
+    public void ans(){
+        if(index >= n){
+            a++;
+            return;
+        }
+        if(arr.get(index) == 0){
+            return;
+        }
+        index++;
+        ans();
+        index++;
+        ans(); 
+    }
+    
+}
